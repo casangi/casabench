e8bde29c9b7fa8e3091b84f01681c1b42d8b0496
Andrew McNichols <amcnicho@nrao.edu>
2023-06-05 15:45:07 -0400

    Remove iter0 version of new bench_alma.BaseMosaic.time_mosaic_cube_eph_pcwdT_restart

--- benchmarks/bench_alma.py
+++ benchmarks/bench_alma.py
@@ -30,15 +30,6 @@ class BaseMosaic(BaseMosaicSetup):
         if (os.path.exists(self.msfile)):
             os.system('rm -rf ' + self.msfile)
 
-    def time_mosaic_cube_eph_pcwdT(self):
-        """Mosaic ephemeris cube imaging with briggsbwtaper - field Venus, spw 45"""
-        ret = tclean(vis=self.msfile, imagename=self.img+"0", field='Venus', spw=['1'], antenna=['0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46'], scan=['7,11'], intent='OBSERVE_TARGET#ON_SOURCE', datacolumn='corrected', imsize=[480, 420], cell=['0.14arcsec'], phasecenter='TRACKFIELD', stokes='I', specmode='cubesource', nchan=948, start='261.7643758544GHz', width='0.2441755MHz', perchanweightdensity=True, gridder='mosaic', mosweight=True, usepointing=False, pblimit=0.2, deconvolver='hogbom', restoration=False, restoringbeam='common', pbcor=False, weighting='briggs', robust=0.5, npixels=0, niter=0, threshold='0.0mJy', nsigma=0.0, usemask='auto-multithresh', sidelobethreshold=2.0, noisethreshold=4.25, lownoisethreshold=1.5, negativethreshold=15.0, minbeamfrac=0.3, growiterations=50, dogrowprune=True, minpercentchange=1.0, fastnoise=False, savemodel='none', parallel=self.parallel, verbose=True)
-    time_mosaic_cube_eph_pcwdT.version = "CAS-14086"
-    time_mosaic_cube_eph_pcwdT.repeat = (1,3,600.0) # 1-3 iterations w/ soft cutoff (no new repeats) past 10m
-    time_mosaic_cube_eph_pcwdT.rounds = 1
-    time_mosaic_cube_eph_pcwdT.min_run_count = 1
-    time_mosaic_cube_eph_pcwdT.timeout = 3600 # conservative 1hr hard cap on single execution, not expected to be reached
-
     def time_mosaic_cube_eph_pcwdT_restart(self):
         """Mosaic ephemeris cube imaging with briggsbwtaper - field Venus, spw 45"""
         ret = tclean(vis=self.msfile, imagename=self.img+"0", field='Venus', spw=['1'], antenna=['0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46'], scan=['7,11'], intent='OBSERVE_TARGET#ON_SOURCE', datacolumn='corrected', imsize=[480, 420], cell=['0.14arcsec'], phasecenter='TRACKFIELD', stokes='I', specmode='cubesource', nchan=948, start='261.7643758544GHz', width='0.2441755MHz', perchanweightdensity=True, gridder='mosaic', mosweight=True, usepointing=False, pblimit=0.2, deconvolver='hogbom', restoration=False, restoringbeam='common', pbcor=False, weighting='briggs', robust=0.5, npixels=0, niter=0, threshold='0.0mJy', nsigma=0.0, usemask='auto-multithresh', sidelobethreshold=2.0, noisethreshold=4.25, lownoisethreshold=1.5, negativethreshold=15.0, minbeamfrac=0.3, growiterations=50, dogrowprune=True, minpercentchange=1.0, fastnoise=False, savemodel='none', parallel=self.parallel, verbose=True)
@@ -48,4 +39,4 @@ class BaseMosaic(BaseMosaicSetup):
     time_mosaic_cube_eph_pcwdT_restart.repeat = (1,3,600.0) # 1-3 iterations w/ soft cutoff (no new repeats) past 10m
     time_mosaic_cube_eph_pcwdT_restart.rounds = 1
     time_mosaic_cube_eph_pcwdT_restart.min_run_count = 1
-    time_mosaic_cube_eph_pcwdT_restart.timeout = 7200 # raise to 2hr hard cap
+    time_mosaic_cube_eph_pcwdT_restart.timeout = 7200 # raise to 2hr hard cap since this is a long test
